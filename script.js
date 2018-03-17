9eee9de2d5fefb36d9c734fd9135fa76c0504776
Student <student@Admins-MacBook-Air.local>
2018-03-17 12:30:17 -0700

    created the shooter project

--- script.js
+++ script.js
@@ -39,7 +39,17 @@ var alienBulletY;
  * select(), and adds event listeners to those elements. Sets initial values of
  * variables by calling resetGame().
  */
-
+function setup() {
+  canvas = createCanvas(500, 400);
+  background(20, 30, 40);
+  gameScreen = select("#game-screen");
+  canvas.parent(gameScreen);
+  shipColor = "#fffa00";
+  shipDiameter = 80;
+  shipX = width / 2;
+  shipY = height - shipDiameter / 2;
+  shipSpeed = 6;
+}
 
 /*
  * gameOver()
@@ -61,13 +71,28 @@ var alienBulletY;
  * This function animates the ship, alien, and both kinds of bullets, but only
  * if the game is running.
  */
-
-
+function draw(){
+  background(20, 30, 40);
+  drawShip();
+}
 /*
  * drawShip()
  * This function draws the player's ship. It also controls the ship's
  * x value by checking if the player is holding down the left or right keys.
  */
+function drawShip() {
+  if(keyIsDown(LEFT_ARROW) && shipX > (shipDiameter / 2)){
+    shipX -= shipSpeed;
+
+  }
+  else if (keyIsDown(RIGHT_ARROW) && shipX < width - shipDiameter /2) {
+    shipX += shipSpeed;
+    }
+
+  fill(shipColor);
+  ellipse(shipX, shipY, shipDiameter, shipDiameter);
+}
+
 
 
 /*
